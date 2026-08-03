437a25629c01a2a11bfb9ca7c98e5b5c6c0a510b
Itamar Turner-Trauring <itamar@itamarst.org>
2026-08-03 16:28:02 -0400

    Add sequential.

--- benchmarks/test_overhead.py
+++ benchmarks/test_overhead.py
@@ -21,8 +21,22 @@ class OrigExecutor(OrigExecutor):
     pass
 
 
+class Sequential:
+    def __init__(self, n_cpus):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return False
+
+    def map(self, func, args):
+        return (func(arg) for arg in args)
+
+
 @pytest.mark.parametrize("function", [noop, spin_100us])
-@pytest.mark.parametrize("executor_factory", [OrigExecutor, SpinExecutor])
+@pytest.mark.parametrize("executor_factory", [OrigExecutor, SpinExecutor, Sequential])
 def test_one_thousand_calls(benchmark, function, executor_factory):
     def run():
         with executor_factory(8) as executor:
